cb8d10caf970b2ab799dbef44708192b69136818
Eyal Bompuis <eyal.bompuis@celadodc-rswl.com>
2023-01-21 22:50:05 +0100

    Remove GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
- 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
-  }
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
